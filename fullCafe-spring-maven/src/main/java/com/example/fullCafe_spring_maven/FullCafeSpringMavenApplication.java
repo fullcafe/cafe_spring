7f74565012d556d459fcc84540f89f720395a6d5
leaf1191 <jwlv111@naver.com>
2024-08-12 21:25:10 +0900

    create project

---
+++ fullCafe-spring-maven/src/main/java/com/example/fullCafe_spring_maven/FullCafeSpringMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullCafe_spring_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullCafeSpringMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullCafeSpringMavenApplication.class, args);
+	}
+
+}
---
+++ fullCafe-spring-maven/src/test/java/com/example/fullCafe_spring_maven/FullCafeSpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullCafe_spring_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullCafeSpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
