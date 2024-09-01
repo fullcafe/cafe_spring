d5db12ea80e68036e00ec11cae28d1bb5f09dcf9
leaf1191 <jwlv111@naver.com>
2024-09-01 13:22:15 +0900

    test: 카페 서비스 테스트 추가

--- fullCafe-spring-maven/src/test/java/com/example/fullCafe_spring_maven/service/cafe/CafeServiceImplTest.java
+++ fullCafe-spring-maven/src/test/java/com/example/fullCafe_spring_maven/service/cafe/CafeServiceImplTest.java
@@ -1,17 +1,30 @@
 package com.example.fullCafe_spring_maven.service.cafe;
 
+import com.example.fullCafe_spring_maven.model.Cafe;
 import com.example.fullCafe_spring_maven.repository.cafe.CafeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
 class CafeServiceImplTest {
     private CafeService cafeService;
     @MockBean
     private CafeRepository cafeRepository;
+
+    private final Cafe cafe = Cafe.builder()
+            .name("cafe")
+            .address("address")
+            .phone("12345")
+            .build();
 
     @BeforeEach
     void setup(){
@@ -21,8 +34,15 @@ class CafeServiceImplTest {
     @Test
     @DisplayName("카페 이름으로 카페 조회 - 서비스")
     void findCafeByCafeName(){
-        // 카페 이름을 먼저 받음
+        //given
+        Optional<Cafe> optionalCafe = Optional.ofNullable(cafe);
+        Mockito.when(cafeRepository.findById(cafe.getName())).thenReturn(optionalCafe);
+        // 카페가 잘 존재
+        Cafe cafe1 = cafeService.findCafeByCafeName(cafe.getName());
+        assertEquals(cafe1,cafe);
         // 카페가 없으면 예외 발생
-        // 카페가 존재하면 들고옴 dto형태로
+        assertThrows(CafeNotFoundException.class,()->{
+            cafeService.findCafeByCafeName("쓰레기 값");
+        });
     }
 }
