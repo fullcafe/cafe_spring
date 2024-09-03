69adfe27b88791b1feee4aa7fc8907b701d6efe7
nisiki <myleskim123@gmail.com>
2024-09-03 22:08:01 +0900

    feat: 주석한줄 추가

--- fullCafe-spring-maven/src/main/java/com/example/fullCafe_spring_maven/controller/bookmark/BookmarkController.java
+++ fullCafe-spring-maven/src/main/java/com/example/fullCafe_spring_maven/controller/bookmark/BookmarkController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+//주석추가
 @RestController
 @RequestMapping("/api/bookmarks")
 public class BookmarkController {
