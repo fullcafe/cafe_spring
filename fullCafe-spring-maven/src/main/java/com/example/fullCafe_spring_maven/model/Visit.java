ec09ebabc856e937d29c8199c9ecedc959e8146f
leaf1191 <jwlv111@naver.com>
2024-09-04 10:28:45 +0900

    feat: Visit create 서비스

--- fullCafe-spring-maven/src/main/java/com/example/fullCafe_spring_maven/model/Visit.java
+++ fullCafe-spring-maven/src/main/java/com/example/fullCafe_spring_maven/model/Visit.java
@@ -14,23 +14,27 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 public class Visit {
     @Id
-    @GeneratedValue
-    private int id;
+    @Column(nullable = false)
+    private String uid;
+    @Id
+    @Column(nullable = false)
+    private String cafeName;
     private int count; // 횟수 디폴트 0
     private boolean writeReview; // 리뷰 작성 여부 디폴트 F
     @Column(nullable = false)
     private LocalDateTime recent; // 최근 방문 시간
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "uid", nullable = false)
+    @JoinColumn(name = "uid")
     private User user;
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "cafeName", nullable = false)
+    @JoinColumn(name = "cafeName")
     private Cafe cafe;
 
     @Override
     public String toString() {
         return "Visit{" +
-                "id=" + id +
+                "uid='" + uid + '\'' +
+                ", cafeName='" + cafeName + '\'' +
                 ", count=" + count +
                 ", writeReview=" + writeReview +
                 ", recent=" + recent +
--- fullCafe-spring-maven/src/main/java/com/example/fullCafe_spring_maven/service/visit/VisitService.java
+++ fullCafe-spring-maven/src/main/java/com/example/fullCafe_spring_maven/service/visit/VisitService.java
@@ -1,4 +1,7 @@
 package com.example.fullCafe_spring_maven.service.visit;
 
+import com.example.fullCafe_spring_maven.model.Visit;
+
 public interface VisitService {
+    void createVisit(Visit visit);
 }
