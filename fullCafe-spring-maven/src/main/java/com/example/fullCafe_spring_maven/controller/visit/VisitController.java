6a2439f8bb9be38e38c28e1c4f902c70a6aec279
leaf1191 <jwlv111@naver.com>
2024-09-04 20:30:55 +0900

    feat: 유저 기반 조회 컨트롤러

--- fullCafe-spring-maven/src/main/java/com/example/fullCafe_spring_maven/controller/visit/VisitController.java
+++ fullCafe-spring-maven/src/main/java/com/example/fullCafe_spring_maven/controller/visit/VisitController.java
@@ -1,5 +1,6 @@
 package com.example.fullCafe_spring_maven.controller.visit;
 
+import com.example.fullCafe_spring_maven.model.dto.visit.ComplexVisitDto;
 import com.example.fullCafe_spring_maven.model.dto.visit.SimpleVisitDto;
 import com.example.fullCafe_spring_maven.service.visit.VisitIntegrationService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,9 +9,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +24,18 @@ public class VisitController {
     public ResponseEntity<SimpleVisitDto> createVisit(@Valid @RequestBody SimpleVisitDto visitDto){
         visitIntegrationService.createVisit(visitDto);
         return new ResponseEntity<SimpleVisitDto>(visitDto, HttpStatus.CREATED);
+    }
+    @GetMapping("/visits/all/{uid}")
+    public List<ComplexVisitDto> findAllVisitByUser(@PathVariable String uid){
+        return visitIntegrationService.findAllVisitByUser(uid);
+    }
+    @GetMapping("/visits/no_review/{uid}")
+    public List<ComplexVisitDto> findNoReviewVisitByUser(@PathVariable String uid){
+        return visitIntegrationService.findNoReviewVisitByUser(uid);
+    }
+    @GetMapping("/visits/most_visit/{uid}")
+    public List<ComplexVisitDto> findMostCountVisitByUser(@PathVariable String uid){
+        return visitIntegrationService.findMostCountVisitByUser(uid);
     }
 
 }
